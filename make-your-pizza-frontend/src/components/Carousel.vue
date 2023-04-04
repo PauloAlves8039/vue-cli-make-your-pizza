cede191b8b0d9a4fe9999ada2bef7e4c7d58b22a
PauloAlves8039 <pj38alves@gmail.com>
2023-04-03 21:17:05 -0300

    Criando o componente carousel

---
+++ make-your-pizza-frontend/src/components/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="carousel-pizza" class="carousel slide" data-bs-ride="carousel">
+
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carousel-pizza" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#carousel-pizza" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#carousel-pizza" data-bs-slide-to="2"></button>
+        </div>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/images/pizza1.jpg" alt="Pizza 1" class="d-block image-carousel">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/pizza2.jpg" alt="Pizza 2" class="d-block image-carousel">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/pizza3.jpg" alt="Pizza 3" class="d-block image-carousel">
+            </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "Carousel"
+}
+</script>
+  
+<style scoped>
+.image-carousel {
+    width: 100%;
+    height: 30rem;
+}
+</style>
--- make-your-pizza-frontend/src/views/HomeView.vue
+++ make-your-pizza-frontend/src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h2 class="title-home">Make Your Pizza</h2>
+    <div class="container">
+      <Carousel />
+    </div>
   </div>
 </template>
 
 <script>
+import Carousel from '@/components/Carousel.vue';
 
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    Carousel
+  }
 }
 </script>
+
+<style scoped>
+.title-home {
+  text-align: center;
+  color: #15B115;
+  font-weight: 700;
+}
+</style>
