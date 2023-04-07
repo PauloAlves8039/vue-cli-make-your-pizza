8e4c21fa398bbad317b0e060d5dd00ae7c02b843
PauloAlves8039 <pj38alves@gmail.com>
2023-04-07 12:19:46 -0300

    Conectando ao banco de dados e inserindo no modal

--- make-your-pizza-frontend/src/components/Modal.vue
+++ make-your-pizza-frontend/src/components/Modal.vue
@@ -10,62 +10,32 @@
                     
                     <div class="mb-1">
                         <label for="name" class="form-label title-label">Nome do Cliente</label>
-                        <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
+                        <input type="text" class="form-control" id="name" name="name" v-model="name">
                     </div>
                     <div class="mb-3">
-                        <label for="name" class="form-label title-label">Selecione o Sabor</label>
-                        <select name="flavor" id="flavor" class="form-select" aria-label="Default select example">
-                            <option selected></option>
-                            <option value="flavor">One</option>
-                            <option value="flavor">Two</option>
-                            <option value="flavor">Three</option>
+                        <label for="name" class="form-label title-label">Escolha o Sabor</label>
+                        <select name="flavor" id="flavor" class="form-select" v-model="flavor">
+                            <option v-for="flavor in flavors" :key="flavor.id" :value="flavor.type">
+                                {{ flavor.type }}
+                            </option>
                         </select>
                     </div>
                     <div class="mb-3">
                         <label for="name" class="form-label title-label">Escolha o Recheio</label>
-                        <select name="filling" id="filling" class="form-select" aria-label="Default select example">
-                            <option selected></option>
-                            <option value="filling">One</option>
-                            <option value="filling">Two</option>
-                            <option value="filling">Three</option>
+                        <select name="filling" id="filling" class="form-select" v-model="filling">
+                            <option v-for="filling in fillings" :key="filling.id" :value="filling.type">{{ filling.type }}</option>
                         </select>
                     </div>
                     
                     <div>
-                        <label class="title-label">Selecione os Opicionais</label>
+                        <label class="title-label">Selecione os Opcionais</label>
                     </div>
                     
-                    <div class="row mt-1">
-                        <div class="col">
-                            <div class="mb-2 form-check">
-                                <input type="checkbox" class="form-check-input" id="chicken">
-                                <label class="form-check-label" for="chicken">Frango</label>
-                            </div>
-                            <div class="mb-2 form-check">
-                                <input type="checkbox" class="form-check-input" id="olive">
-                                <label class="form-check-label" for="olive">Azeitona</label>
-                            </div>
-                            <div class="mb-2 form-check">
-                                <input type="checkbox" class="form-check-input" id="artichoke">
-                                <label class="form-check-label" for="artichoke">Alcachofra</label>
-                            </div>
+                    <div class="row mt-1 check-optional">
+                        <div class="col-sm-4 mb-2 form-check form-check-inline" v-for="optional in optionalData" :key="optional.id">
+                            <input type="checkbox" class="form-check-input" :value="optional.type">
+                            <span>{{ optional.type }}</span>
                         </div>
-
-                        <div class="col">
-                            <div class="mb-2 form-check">
-                                <input type="checkbox" class="form-check-input" id="broccoli">
-                                <label class="form-check-label" for="broccoli">Brócolis</label>
-                            </div>
-                            <div class="mb-2 form-check">
-                                <input type="checkbox" class="form-check-input" id="mushrooms">
-                                <label class="form-check-label" for="mushrooms">Cogumelos</label>
-                            </div>
-                            <div class="mb-2 form-check">
-                                <input type="checkbox" class="form-check-input" id="oregano">
-                                <label class="form-check-label" for="oregano">Orégano</label>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -83,12 +53,39 @@
   
 <script>
 export default {
-    name: "Modal"
+    name: "Modal",
+    data() {
+        return {
+            flavors: null,
+            fillings: null,
+            optionalData: null,
+            name: null,
+            flavor: null,
+            filling: null,
+            optional: [],
+            status: 'Solicitado',
+            message: null
+        }
+    },
+    methods: {
+        async getIngredients() {
+            const urlIngredients = "http://localhost:3001/ingredients";
+            const request = await fetch(urlIngredients);
+            const data = await request.json();
+
+            this.flavors = data.flavors;
+            this.fillings = data.fillings;
+            this.optionalData = data.optional;
+        }
+    },
+    mounted() {
+        this.getIngredients();
+    }
 }
 </script>
   
 <style scoped>
-#modal-title-label{
+#modal-title-label {
     margin-left: auto;
     color: #15B115;
     font-weight: 700;
@@ -96,5 +93,9 @@ export default {
 
 .title-label {
     font-weight: 600;
+}
+
+.check-optional {
+    margin-left: 0;
 }
 </style>
