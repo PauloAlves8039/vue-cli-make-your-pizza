5822e2a9e47b52b3115d726bef2995f6e6c410a9
PauloAlves8039 <pj38alves@gmail.com>
2023-04-24 21:22:57 -0300

    Implementando o componente Spinner

--- make-your-pizza-frontend/src/App.vue
+++ make-your-pizza-frontend/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <Navbar :logo="logo_src" :alt="app_name" />
   <router-view />
+  <Spinner />
   <Footer />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import Spinner from './components/Spinner.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Footer
+    Footer,
+    Spinner
   }
 }
 </script>
---
+++ make-your-pizza-frontend/src/components/Spinner.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="spinner-body" v-if="loading">
+        <div class="spinner-grow text-success spinner-boder" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Spinner",
+    data() {
+    return {
+      loading: true
+    }
+  },
+  async mounted() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    this.loading = false;
+  },
+}
+</script>
+<style scoped>
+.spinner-body {
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    transition: all 0.2s;
+    opacity : 0.7;
+    z-index: 9999;
+    background-color: var(--var-system-color-black);
+}
+
+.spinner-boder {
+    width: 5rem;
+    height: 5rem;
+    background-color: var(--var-system-color-success);
+}
+</style>
